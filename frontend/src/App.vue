92cc6deebd6be91ca1b21662ac846cda41ccef8f
M00845272 <SL1558@live.mdx.ac.uk>
2024-03-19 21:44:13 +0000

    seperate vue component and styling for login functionality

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,95 +1,36 @@
-<script setup lang="ts">
-  import { Authenticator } from "@aws-amplify/ui-vue";
-
-  import { Amplify } from 'aws-amplify';
-  import awsconfig from './aws-exports';
-
-  Amplify.configure(awsconfig);
-</script>
-
 <template>
-  <authenticator>
-    <template v-slot:sign-up-header>
-      <h3
-        class="amplify-heading"
-        style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl)"
-      >
-        Create a new account
-      </h3>
-    </template>
-    <template v-slot:header>
-      <div style="padding: var(--amplify-space-large); text-align: center">
-        <img
-          class="amplify-image"
-          alt="Amplify logo"
-          src="https://docs.amplify.aws/assets/logo-dark.svg"
-        />
-      </div>
-    </template>
-    <template v-slot:sign-in-header>
-      <h3
-        class="amplify-heading"
-        style="padding: var(--amplify-space-xl) 0 0 var(--amplify-space-xl)"
-      >
-        Sign in to your account
-      </h3>
-    </template>
-    <template v-slot="{ user, signOut }">
-      {{ user }}
-      <h1>Hello {{ user.username }}!</h1>
-      <button @click="signOut" variation="primary">Sign Out</button>
-    </template>
-    <template v-slot:footer>
-      <div style="padding: var(--amplify-space-large); text-align: center">
-        <p class="amplify-text" style="color: var(--amplify-colors-neutral-80)">
-          © All Rights Reserved
-        </p>
-      </div>
-    </template>
-  </authenticator>
+  <div id="app">
+      <header>
+          <div class="navbar navbar-expand-lg bg-dark navbar-dark app-header">
+              <div class="navbar-header">
+                  <h1>{{ sitename }}</h1>
+              </div>
+              <div class="nav navbar-nav navbar-right cart">
+                  <button type="button" class="btn btn-primary btn-lg" v-on:click="logout">
+                      Logout
+                  </button>
+              </div>
+          </div>
+      </header>
+      <main>
+          <component :is="currentView" ></component>
+      </main>
+  </div>
 </template>
 
+<script>
+import Authentication from "./components/Authentication.vue";
+import Home from "./components/Home.vue";
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-[data-amplify-authenticator] {
-  --amplify-components-button-primary-background-color: var(
-    --amplify-colors-brand-secondary-80
-  );
-  --amplify-components-button-primary-hover-background-color: var(
-    --amplify-colors-brand-secondary-90
-  );
-  --amplify-components-button-primary-focus-background-color: var(
-    --amplify-colors-brand-secondary-90
-  );
-  --amplify-components-button-primary-active-background-color: var(
-    --amplify-colors-brand-secondary-100
-  );
-  --amplify-components-button-link-color: var(
-    --amplify-colors-brand-secondary-80
-  );
-  --amplify-components-button-link-hover-color: var(
-    --amplify-colors-brand-secondary-90
-  );
-  --amplify-components-button-link-focus-color: var(
-    --amplify-colors-brand-secondary-90
-  );
-  --amplify-components-button-link-active-color: var(
-    --amplify-colors-brand-secondary-100
-  );
-  --amplify-components-button-link-active-background-color: transparent;
-  --amplify-components-button-link-focus-background-color: transparent;
-  --amplify-components-button-link-hover-background-color: transparent;
-}
-</style>
+export default {
+    name: "App",
+    data() {
+        return {
+            sitename: "MoodTracker",
+            currentView: Authentication,
+        }
+    },
+    components: { Authentication, Home },
+
+  };
+</script>
---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <div class="">
+            Home page
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home",
+}
+</script>
