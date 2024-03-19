66b3fcfadc21b616f532c8de023b49c7c7bd566e
M00845272 <SL1558@live.mdx.ac.uk>
2024-03-19 21:23:48 +0000

    authentication functionality with vue + AWS Amplify

---
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifya4cc1441": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
