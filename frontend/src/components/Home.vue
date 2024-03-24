dc46a354162adeb79ea875b5fedd74655e664838
M00845272 <SL1558@live.mdx.ac.uk>
2024-03-24 23:41:10 +0000

    add example pie and timeseries charts

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,8 +1,18 @@
 <template>
     <div>
         <div class="home">
-            <h1>Home Page....under construction...</h1>
-            <h2>Welcome!! </h2>
+            <div class="card">
+                <div class="card-header">Mood Trend</div>
+                <div class="card-body">
+                    <div id='timeseries-chart'></div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header">Mood Sentiment</div>
+                <div class="card-body">
+                    <div id='pie-chart'></div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -13,5 +23,45 @@ const auth = useAuthenticator();
 
 export default {
     name: "Home",
+    methods: {
+        showMoodTrend() {
+            var data = [
+                {
+                    x: ['2024-03-04', '2024-03-05', '2024-03-06', '2024-03-07', '2024-03-08', '2024-03-09'],
+                    y: [1, -1, 2, 1, 0, -2],
+                    type: 'scatter'
+                }
+            ];
+            var layout = {
+                yaxis: {
+                    tickvals: [-2, -1, 0, 1, 2],
+                    ticktext: ['Aweful', 'Bad', 'Meh', 'Good', 'Awesome'],
+                }
+            };
+
+            Plotly.newPlot('timeseries-chart', data, layout, { displaylogo: false }, { pan: false }, { responsive: true });
+        },
+        showMoodSentiment() {
+            var data = [{
+                values: [10, 20, 30, 30, 10],
+                labels: ['Aweful', 'Bad', 'Meh', 'Good', 'Awesome'],
+                type: 'pie',
+                textinfo: "label+percent",
+            }];
+
+            var layout = {
+                height: 500,
+                width: 500,
+                showlegend: 'false',
+                displaylogo: 'false'
+            };
+
+            Plotly.newPlot('pie-chart', data, layout, { displaylogo: false }, { displayModeBar: false });
+        }
+    },
+    mounted() {
+        this.showMoodSentiment();
+        this.showMoodTrend();
+    }
 }
 </script>
